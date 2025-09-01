5140c2ef1edc5770156661eb6bc6709ee25282ac
Community-Programmer <sarthakpatel230204@gmail.com>
2025-09-01 09:25:03 +0000

    Initialize CodeIgniter 4 backend structure with environment configuration, PHPUnit setup, and example tests

---
+++ backend/app/Models/UserModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    protected $table = 'auth_user';
+    protected $allowedFields = ['email', 'first_name', 'last_name', 'password'];
+    protected $beforeInsert = ['hashPassword'];
+
+    protected function hashPassword(array $data) {
+        if (isset($data['data']['password']))
+            $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+        return $data;
+    }
+}
